05bdeeb48dad5212b24f8af0ebd5ded99dd69a2c
Stef-GMS <stephanie@GeekMeSpeak.com>
2025-11-13 13:32:16 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image_resize
+
+An app to enable users to select one or more images to resize.
---
+++ android/app/src/main/kotlin/com/geekmespeak/image_resize/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.geekmespeak.image_resize
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
